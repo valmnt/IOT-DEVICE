9d3782c919f8bd48785d46764026def09bcbc440
MONT <valentin.mont@ynov.com>
2020-06-05 11:56:31 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var iotf = require("ibmiotf");
+var config = require("./device.json");
+
+var deviceClient = new iotf.IotfDevice(config);
+
+deviceClient.log.setLevel('debug');
+
+deviceClient.connect();
+
+deviceClient.on('connect', function() {
+    console.log("connected");
+    setInterval(function function_name () {
+      deviceClient.publish('event_1', 'json', '{"temp":' + 4 +'}');
+    },2000);
+  });
